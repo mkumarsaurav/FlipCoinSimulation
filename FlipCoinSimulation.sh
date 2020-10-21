b1c1a46c4f1e01eb03cf659a527d1af97764deb8
saurav <kumar00914@gmail.com>
2020-10-21 15:51:18 +0530

    Modified the code to continue flipping till either reaches 21 and display the winner and won by margin

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,8 +2,8 @@
 echo "Welcome to Flip Coin Simulation Program"
 tails=0;
 heads=0;
-read -p "Enter how many times to flip the coin: " n
-for (( i=0 ; i<$n ; i++ ))
+i=0;
+while [ $tails -lt 21 -a $heads -lt 21 ]
 do
 	TossCheck=$((RANDOM%2));
 	if [ $TossCheck -eq 0 ];
@@ -15,5 +15,10 @@ do
 		echo Heads Win;
 	fi
 done
-echo "Number of times Heads won: " $heads
-echo "Number of times Tails won: " $tails
+echo "Heads Count :"$heads "and Tails Count : " $tails;
+if [ $heads -gt $tails ]
+then
+   echo "Heads won by: " $(($heads-$tails));
+else
+   echo "Tails won by: "$(($tails-$heads));
+fi
